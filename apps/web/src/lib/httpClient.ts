757bb1ed1b2a47a5f701aceb41662d562d469bb4
Vitor Daniel <vitor036daniel@gmail.com>
2025-05-03 21:21:26 -0300

    fix

--- apps/web/src/lib/httpClient.ts
+++ apps/web/src/lib/httpClient.ts
@@ -3,7 +3,7 @@ import { treaty } from '@elysiajs/eden'
 import { createAuthClient } from "better-auth/react"
 import { emailOTPClient, apiKeyClient, passkeyClient } from "better-auth/client/plugins"
 
-const { api: client } = treaty<App>("/api")
+const { api: client } = treaty<App>(process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000")
 
 export const authClient = createAuthClient({
   basePath: "/api/auth",
