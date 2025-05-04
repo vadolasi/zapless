a25b6723a543f1d53eb34e8b5994a64e0537d275
Vitor Daniel <vitor036daniel@gmail.com>
2025-05-03 21:00:05 -0300

    fix

--- apps/web/src/lib/httpClient.ts
+++ apps/web/src/lib/httpClient.ts
@@ -3,16 +3,9 @@ import { treaty } from '@elysiajs/eden'
 import { createAuthClient } from "better-auth/react"
 import { emailOTPClient, apiKeyClient, passkeyClient } from "better-auth/client/plugins"
 
-let url = "http://localhost:3000"
-
-if (window !== undefined) {
-  url = window.location.origin
-}
-
-const { api: client } = treaty<App>(url)
+const { api: client } = treaty<App>("/api")
 
 export const authClient = createAuthClient({
-  baseURL: url,
   basePath: "/api/auth",
   plugins: [
     emailOTPClient(),
